9e91ac4b72d23ff35d002ec373e0c7d2f33dd263
SaidTheCoder <71198810+SaidTheCoder@users.noreply.github.com>
2022-08-25 19:30:31 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -4,36 +4,20 @@ const server = require("http").Server(app);
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
-const { v4: uuidv4 } = require("uuid");
-
 const io = require("socket.io")(server, {
     cors: {
         origin: '*'
     }
 });
 
-const { ExpressPeerServer } = require("peer");
-const peerServer = ExpressPeerServer(server, {
-    debug: true,
-});
-
-app.use("/peerjs", peerServer);
-
 app.get("/", (req, res) => {
-    res.redirect(`/${uuidv4()}`);
-});
-
-app.get("/:room", (req, res) => {
-    res.render("index", { roomId: req.params.room });
+    res.render("index");
 });
 
 io.on("connection", (socket) => {
-    socket.on("join-room", (roomId, userId, userName) => {
-        socket.join(roomId);
-        socket.on("message", (message) => {
-            io.to(roomId).emit("createMessage", message, userName);
-        });
+    socket.on("message", (message, user) => {
+        io.emit("createMessage", message, user);
     });
 });
 
-server.listen(process.env.PORT || 3030);
+server.listen(4040);
